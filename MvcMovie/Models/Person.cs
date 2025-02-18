689426bc1ef3976d14502e11460cbd92b0cb6d08
hanhvu31 <kumake2k3@gmail.com>
2025-02-19 03:30:22 +0700

    Update_Code

---
+++ MvcMovie/Models/Person.cs
@@ -0,0 +1,10 @@
+namespace MvcProject.Models
+{
+    public class Person
+    {
+        public required string PersonId { get; set; }
+        public required string FullName { get; set; }
+        public required string Address { get; set; }
+
+    }
+}
